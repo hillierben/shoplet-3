a5978729f6d9b44dc8a4900d9b904399aabfe2de
hillierben <hillierben@gmail.com>
2023-11-05 08:36:18 +0000

    pre-production

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Logo from "./Logo";
 import Logo2 from "./Logo2";
 import CategoryBar from "./CategoryBar";
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Paginate from "./components/Paginate";
 import ProductList from "./components/ProductList";
 import ProductList2 from "./components/ProductList2";
 
@@ -5,11 +6,11 @@ import ProductList2 from "./components/ProductList2";
 export default function Home() {
   return (
     <div>
-
-    <div className="pge-prd-list">
-      <ProductList />
-    </div>
-
+      <Paginate />
+      <div className="pge-prd-list">
+        <ProductList />
+      </div>
+      <Paginate />
   </div>
    
   )
